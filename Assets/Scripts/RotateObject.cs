fb9e3b38d7c45fc8ddca51fa172862e77da037ea
ShamanCoder <simon.spasskiy@protonmail.com>
2020-04-19 10:50:14 +0000

    Modified version of Unity Building Blocks
    Most of the scripts remain untouched, except for the TargetHandler, Controls and RotateObject. Removed cursor show and hide from TargetHandler. Changed mouse delta to mouse position in Controls. Rotate Object has cursor show and hide removed and is adjusted with the new rotation system. New rotation system requires a new cursor GameObject, which is put into a Turret prefab.

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace DapperDino.BuildingBlocks
+{
+    public class RotateObject : MonoBehaviour
+    {
+        [SerializeField] private float offset = 0f;
+        [SerializeField] private float speed = 0f;
+        [SerializeField] private float distance = 0f;
+        [SerializeField] private GameObject cursor;
+
+        private Ray aim;
+
+        private void Awake()
+        {
+
+        }
+
+        public void Rotate(CallbackContext ctx)
+        {
+            if (!ctx.performed) { return; }
+
+            RaycastHit hit;
+            var aimInput = ctx.ReadValue<Vector2>();
+            
+            aim = Camera.main.ScreenPointToRay(aimInput);
+            Vector3 mousePosition = aimInput;
+            mousePosition.y += offset;
+            mousePosition.z = distance;
+            cursor.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            if(Physics.Raycast(aim, out hit, Mathf.Infinity))
+            {
+                float finalOffset;
+                finalOffset = (transform.position - hit.point).magnitude * offset;
+                cursor.transform.position = new Vector3(cursor.transform.position.x, cursor.transform.position.y + finalOffset, cursor.transform.position.z);
+                transform.LookAt(cursor.transform);
+            }
+            else
+            {
+                transform.LookAt(cursor.transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TurretMovement.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace DapperDino.BuildingBlocks
+{
+    public class TurretMovement : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 5f;
+        [SerializeField] private Vector2 limits = new Vector2(-4f, 4f);
+
+        private bool Axis = false;
+        private bool Vector2 = false;
+
+        private float previousInputFloat;
+        private Vector2 previousInputVector2;
+
+        private void Update() => Move();
+
+        public void MoveInput(CallbackContext ctx)
+        {
+            if (ctx.performed)
+            {
+                if (ctx.valueType.ToString() == "System.Single")
+                {
+                    previousInputFloat = ctx.ReadValue<float>();
+                    if(!Axis)
+                    {
+                        Axis = true;
+                    }
+                }               
+                else if (ctx.valueType.ToString() == "UnityEngine.Vector2")
+                {
+                    previousInputVector2 = ctx.ReadValue<Vector2>();
+                    if (!Vector2)
+                    {
+                        Vector2 = true;
+                    }
+                }
+                return;
+            }
+
+            if (ctx.canceled)
+            {
+                previousInputFloat = 0f;
+                previousInputVector2 = UnityEngine.Vector2.zero;
+            }
+        }
+
+        private void Move()
+        {
+            if (Axis)
+            {
+                Move1D();
+            }
+            else if (Vector2)
+            {
+                Move2D();
+            }
+        }
+
+        private void Move1D()
+        {
+            float movement = previousInputFloat * movementSpeed * Time.deltaTime;
+            float newXValue = Mathf.Clamp(transform.position.x + movement, limits.x, limits.y);
+
+            transform.position = new Vector3(newXValue, transform.position.y, transform.position.z);
+        }
+
+        private void Move2D()
+        {
+
+        }
+    }
+}
